f8ec88bd771a2eba6401abf4594743dbfd96aa76
Ben Allen <benjamin.q.allen@gmail.com>
2017-05-19 03:01:12 +0000

    Added custom Google Fonts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+#PageController inherrits attributes from ApplicationController
+#Home
 class PagesController < ApplicationController
     def home
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+#Home page needs to point to home method in pages controller
+#first to be activated. Routes checks for controller for home action (which is html)
 Rails.application.routes.draw do
     root to: 'pages#home'
 end
