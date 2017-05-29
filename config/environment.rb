d42f0dfeaade690fa97a099753fdc483e3226aa0
Ben Allen <benjamin.q.allen@gmail.com>
2017-05-29 12:49:19 +0000

    Tryin again with mailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'powerful-bastion-48613.herokuapp.com/',
+  :domain         => 'cryptic-waters-92322.herokuapp.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
