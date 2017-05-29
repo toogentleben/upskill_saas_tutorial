08966e9676c6600996324caf7f4a96b619677c24
Ben Allen <benjamin.q.allen@gmail.com>
2017-05-29 00:19:26 +0000

    Updated url in enviroment file

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'arcane-bayou-18057.herokuapp.com/',
+  :domain         => 'powerful-bastion-48613.herokuapp.com/',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
