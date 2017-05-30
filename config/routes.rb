bab0463140f4b560787cf4f36ff6a80ed0750523
Ben Allen <benjamin.q.allen@gmail.com>
2017-05-30 17:34:28 +0000

    Installed devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 #Home page needs to point to home method in pages controller
 #first to be activated. Routes checks for controller for home action (which is html)
 Rails.application.routes.draw do
+  devise_for :users
     root to: 'pages#home'
     get 'about', to: 'pages#about'
     resources :contacts, only: :create
